97eafd98ee659d0b5ef4b8f8de13f0719545347c
Sagar Singh <sagarsingh8723@gmail.com>
2026-02-21 14:06:37 +0530

    Update Firebase configuration with actual values

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,14 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace placeholder values with your actual Firebase config
 // You can find these in Firebase Console -> Project Settings -> Your Apps
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyBwvUI61XXmTLWvR0iUzSgXH4leeCS4aQk",
+  authDomain: "fleet-33608.firebaseapp.com",
+  projectId: "fleet-33608",
+  storageBucket: "fleet-33608.firebasestorage.app",
+  messagingSenderId: "14302289624",
+  appId: "1:14302289624:web:53bbc2084d9b5a44ec01ad",
+  measurementId: "G-458KN9FCP3"
 };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export default app;
